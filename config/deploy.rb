de558d0ddc19cd0112a2b60840e1184f872203cd
Andy <andycom12000@gmail.com>
2015-04-02 16:48:00 +0800

    modified Capfile, deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :deploy_to, '/home/deploy/helloWorld'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml', 'config/secrets.yml'}
+set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
